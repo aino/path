97484cb421722482f17533e38e1fc6a566ff30fb
David Hellsing <david@David-Hellsings-MacBook.local>
2011-02-12 21:57:35 +0100

    v 1.2

--- README.rst
+++ README.rst
@@ -2,53 +2,23 @@
 Path
 ====
 
-:path is a useful jQuery selector. It filters out anchor elements where the href attribute matches the window.location path, including ancestors.
-
 Adding active classes is very common in navigational elements in order to style an 'active state'. This is normally added server-side using sometimes complicated logic.
 
-As a compelling alternative for front-end developers and designers, :path uses javascript and jQuery to parse the window.location and match it with href attributes. The plugin works recursively, meaning it can take the entire path to your page, match it with anchor hrefs and return navigational anchor elements that points to the page's ancestors, as well as itself. Use CSS or jQuery to parse and style the active elements properly.
+As a compelling alternative for front-end developers and designers, ``:path`` uses javascript and jQuery to parse the window.location and match it with href attributes. The plugin works recursively, meaning it can take the entire path to your page, match it with anchor hrefs and return navigational anchor elements that points to the page's ancestors, as well as itself. Use CSS or jQuery to parse and style the active elements properly.
 
-:path is a pseudo-class, so you can continue the chain for easy manipulation.
+``:path`` is a pseudo-class, so you can continue the chain for easy manipulation.
 
 In addition to the :path selector, the package also includes a :current pseudo-class that targets the anchor that links to the current page.
-
-
-Features
-========
-
-Works on all relative href paths (including '../') as well as absolute paths
-Returns empty if the href points to root
-Ignores a custom array of file names, such as index.html
-Works on query strings, such as ?s1=home&s2=blog
-includes a :current pseudo-class so you can style the anchor that links to the current page location
-Tested in Safari 3, Firefox 2, MSIE 6, MSIE 7, Opera 9
 
 Usage
 =====
 
-Since :path is a pseudo-class, implementation is dead-easy. The following example will add an 'active' class to the filtered anchors parent inside every <ul>:
+Since :path is a pseudo-class, implementation is dead-easy. The following example will add an 'active' class to the filtered anchors parent inside every <ul>::
 
-$('ul a:path').parent().addClass('active');
+    $('ul a:path').parent().addClass('active');
 
-:path also comes with the similar pseudo-class :current that targets the current page. Since Nielsen says that you should never have a link that points to the current page, the following example replaces the anchor that points to the current page with it's containing text:
+``:path`` also comes with the similar pseudo-class, ``:current`` that targets the current page::
 
-$('ul a:current').each(function() { 
-    $(this).replaceWith($(this).text()); 
-});
+    $('ul a:current').parent().addClass('current');
 
-Options
-=======
-
-:path comes with two options
-
-strictQuery - a boolean that sets if the script should ignore the order of query strings or not. Example: $.path.strictQuery = false; means that ?a=b&c=d equals ?c=d&a=b. Defaults to true.
-ignoreFiles - an array of file names to ignore when comparing, so that / equals /index.html and /index.php etc. Defaults to ['index.htm','index.html','index.shtml','index.cgi','index.php'].
-You can override the defaults like this (example):
-
-$.path.strictQuery = false;
-
-$.path.ignoreFiles = ['default.asp','home.html'];
-
-License
-=======
-:path is currently licensed under the GPL license.
+The difference is that while ``:path`` filters out all anchors pointing to parent locations, ``:current`` only filters out the anchors pointing to the exact same page that you are at.
--- path.js
+++ path.js
@@ -1,149 +1,103 @@
-/*!
- * Copyright (c) 2010, Aino - http://aino.se/
- * Usage: $('a:path').addClass('active');
+/*
+ * Path v 1.2 2011-02-12
+ *
+ * Copyright (c) 2011, Aino
+ * Licensed under the MIT license.
  */
-(function($){
-    var path = {
-	
-    	// array of files to ignore
-    	ignoreFiles : ['index.htm','index.html','index.shtml','index.cgi','index.php'],
-	
-    	// strict mode boolean
-    	strictQuery : true,
-	
-    	// regexp for ignored file names
-    	ignore : function() {
-    		return new RegExp('('+this.ignoreFiles.join('|')+')','i');
-    	},
-	
-    	// grab and parse the location
-    	window : function() {
-		
-    		// define window.location as a short variable
-    		var _l = window.location;
-		
-    		// grab the window path, split & and parse
-    		var _w = _l.href.cleanPath(this.ignore()).split('/');
-		
-    		// grab the query string, split & sort if not in strict mode
-    		var _q = _l.search.substring(1).length > 0 ? _l.search.substring(1).split('&') : [];
-    		if (!this.strictQuery) { _q.sort(); }
-		
-    		// merge the arrays
-    		return _w.concat(_q);
-    	},
-	
-    	// grab and parse the anchor
-    	anchor : function(_this) {
-		
-    		// grab the hrefs
-    		var _org = _this.getAttribute('href',1);
-		
-    		// return false if node is not anchor or href is not present
-    		if (!_org || _this.nodeName != 'A') { return false; }
-		
-    		var _l = window.location;
-		
-    		// parse href
-    		var _href = _org.absUrl().cleanPath(this.ignore());
-		
-    		// return if href is root
-    		if (_href === (_l.protocol + '//' + _l.hostname).replace(/www\./,'').noSlash()) {
-    			return false;
-    		}
 
-    		// split href into path & query
-    		var _s = _href.split("?");
-    		var _a = _s[0].noSlash().split('/');
-    		var _q = _s.length > 1 ? _s[1].split('&') : [];
-		
-    		// sort query if not in strict mode
-    		if (!this.strictQuery) { _q.sort(); }
-		
-    		// merge the arrays
-    		return _a.concat(_q);
-		
-    	},
-	
-    	compareArray : function(o,t) {
-    		if (o.length != t.length) { return false; }
-    		for (var i = 0; i < t.length; i++) {
-    			if (o[i] !== t[i]) { 
-    				return false;
-    			}
-    		}
-    		return true;
-    	},
-	
-    	// match the anchor with window
-    	match : function(_this) {
-	    
-    	    if ($(_this).attr('href') == '#') {
-    	        return false;
-    	    }
-		
-    		var _a = this.anchor(_this);
-    		var _w = this.window();
-		
-    		// check if root
-    		if(_this.pathname.split('/').length < 3) {
-    		    return this.compareArray(_w,_a);
-    		}
-    		// compare and return
-    		return (_w.length < _a.length) ? false : this.compareArray(_w.slice(0,_a.length),_a);
-		
-    	},
-	
-    	current : function(_this) {
-	    
-    	    if ($(_this).attr('href') == '#') {
-    	        return false;
-    	    }
+(function($) {
+    
+    var window = this,
+        
+        absUrl = function( url ) {
+            
+            var l = window.location, h, p, f, i, n;
+            
+            if ( /^\w+:/.test( url ) ) {
+                return url.toString();
+            }
+            
+            h = l.protocol + '//' + l.host;
+            if ( url.indexOf( '/' ) === 0 ) {
+                return h + url.toString();
+            }
 
-    		var _a = this.anchor(_this);
-    		var _w = this.window();
-		
-    		// compare and return
-    		//console.log(_this,_w,_a,this.compareArray(_w,_a))
-    		return this.compareArray(_w,_a);
-    	}
-    };
+            p = l.pathname.replace( /\/[^\/]*$/, '' );
+            f = url.match(/\.\.\//g);
 
+            if ( f ) {
+                n = url.substring(f.length * 3);
+                for ( i = f.length; i--; ) {
+                    p = p.substring(0, p.lastIndexOf('/'));
+                }
+            } else {
+                n = url.toString();
+            }
+            return h + p + '/' + n;
+            
+        },
+        
+        compareArray = function( o, t ) {
+            
+            var i;
+            
+            if ( o.length !== t.length ) { 
+                return false; 
+            }
+            
+            for ( i = 0; i < t.length; i++ ) {
+                if ( o[i] !== t[i] ) {
+                    return false;
+                }
+            }
+            return true;
+        },
 
-    // Extend jQuery with the :path selector
-    $.extend($.expr[":"], { path : function(a) { return path.match(a); } } );
+        trim = function( url ) {
+            return url.replace(/#.*$/,'').replace(/\?.*/,'').replace(/\/$/,'');
+        },
 
-    // Extend jQuery with the :current selector
-    $.extend($.expr[":"], { current : function(a) { return path.current(a); } } );
+        location = trim( window.location.href ).split('/'),
 
-    String.prototype.noSlash = function() {
-    	return this.lastIndexOf('/') === this.length-1 || this.lastIndexOf('#') === this.length-1 ? this.substr(0,this.length-1) : this.toString();
-    };
+        isAnchor = function( elem ) {
+            return !!( elem.href && elem.nodeName.toUpperCase() === 'A' );
+        },
 
+        href = function( elem ) {
+            return trim( absUrl( elem.href ) ).split('/');
+        };
+        
+    $.extend( $.expr[":"], {
+        
+        path : function( elem ) {
+            
+            if ( !isAnchor( elem ) ) {
+                return false;
+            }
+            
+            var anchor = href( elem );
+            
+            // if anchor points to root, activate if arrays are equal
+            if ( anchor.length === 3) {
+                return compareArray( location, anchor );
+            }
+            
+            return ( location.length < anchor.length ) ? false : 
+                compareArray( location.slice( 0, anchor.length ), anchor );
+            
+        },
+        
+        current: function( elem ) {
+            
+            if ( !isAnchor( elem ) ) {
+                return false;
+            }
+            
+            var anchor = href( elem );
+            
+            return compareArray( location, anchor );
+            
+        }
+    });
 
-    String.prototype.absUrl = function() {
-    	var l = window.location, h, p, f, i;
-    	if (/^\w+:/.test(this)) {
-    		return this.toString();
-    	}
-    	h = l.protocol + '//' + l.host;
-    	if (this.indexOf('/') === 0) {
-    		return h + this.toString();
-    	}
-    	p = l.pathname.replace(/\/[^\/]*$/, '');
-    	f = this.match(/\.\.\//g);
-    	if (f) {
-    		var n = this.substring(f.length * 3);
-    		for (i = f.length; i--;) {
-    			p = p.substring(0, p.lastIndexOf('/'));
-    		}
-    	} else {
-    		n = this.toString();
-    	}
-    	return h + p + '/' + n;
-    };
-
-    String.prototype.cleanPath = function(ignore) {
-    	return this.replace(/www\./i,'').replace(ignore,'').replace(/\.\//,'').noSlash().toString();
-    };
-})(jQuery);
+}( jQuery ));
